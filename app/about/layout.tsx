bce6cfed67603b135fa18ae342615011644eb8d3
NjengaFelix <felixnjunge@rocketmail.com>
2023-08-24 14:21:59 +0300

    git init

---
+++ app/about/layout.tsx
@@ -0,0 +1,15 @@
+import styles from './style.module.css'
+export default function AboutLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <nav>About Navbar</nav>
+            <main className={styles.main}>
+                {children}
+            </main>
+        </>
+    )
+}
